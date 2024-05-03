1522562bd0d6e6fe9edff03d907508f211f659b6
John Källström <johnkaellstrom@gmail.com>
2024-05-03 14:30:42 +0200

    Validate ssn

--- GarageThree.Web/Validations/SocialSecurityNumberAttribute.cs
+++ GarageThree.Web/Validations/SocialSecurityNumberAttribute.cs
@@ -1,14 +1,23 @@
-﻿namespace GarageThree.Web.Validations
+﻿using System.Text.RegularExpressions;
+
+namespace GarageThree.Web.Validations
 {
     public class SocialSecurityNumberAttribute() : ValidationAttribute
     {
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
             var configuration = validationContext.GetRequiredService<IConfiguration>();
-            string errorMessage = configuration.GetValue<string>("Validations:SSN:ErrorMessage")!;
-            int maxLength = configuration.GetValue<int>("Validations:SSN:MaxLength");
 
-            return new ValidationResult(errorMessage);
+            string errorMessage = configuration.GetValue<string>("Validations:SSN:ErrorMessage")!;
+            int correctLength = configuration.GetValue<int>("Validations:SSN:CorrectLength");
+
+            string? ssn = (string?)value;
+
+            if (string.IsNullOrWhiteSpace(ssn)) return new ValidationResult(errorMessage);
+            if (!ssn.Length.Equals(correctLength)) return new ValidationResult(errorMessage);
+            if (Regex.IsMatch(ssn, @"(\D)")) return new ValidationResult(errorMessage);
+
+            return ValidationResult.Success;
         }
     }
 }
