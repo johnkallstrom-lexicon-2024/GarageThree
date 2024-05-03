bb7d76dbd1033487ac1d384bff8e2330e09ce454
suny-am <visualarea.1@gmail.com>
2024-05-03 07:23:30 +0000

    Update MemberIndexViewModel with static properties + vehiclecount property

--- GarageThree.Web/ViewModels/Member/MemberIndexViewModel.cs
+++ GarageThree.Web/ViewModels/Member/MemberIndexViewModel.cs
@@ -2,11 +2,16 @@
 
 public class MemberIndexViewModel
 {
+    public static string SearchTerm { get; set; } = default!;
+    public static int PageSize { get; set; } = default!;
+    public static int CurrentPage  { get; set; } = default!;
+
     public int Id { get; set; }
     public string Avatar { get; set; } = default!;
     public string Username { get; set; } = default!;
     public string FirstName { get; set; } = default!;
     public string LastName { get; set; } = default!;
-    public string Email { get; set; } = default!;
     public string Name => $"{FirstName} {LastName}";
+    public string Email { get; set; } = default!;
+    public int VehicleCount { get; set; } = default!;
 }
