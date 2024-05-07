894aae66e6daab47e7d680ed4ac54ec6f2ff7049
John Källström <johnkaellstrom@gmail.com>
2024-05-07 19:48:21 +0200

    Checkout vm

--- GarageThree.Web/ViewModels/Vehicle/VehicleCheckoutViewModel.cs
+++ GarageThree.Web/ViewModels/Vehicle/VehicleCheckoutViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class VehicleCheckoutViewModel
     {
+        public TimeSpan ParkingPeriod { get; set; }
+        public decimal TotalParkingPrice { get; set; }
+        public string Garage { get; set; } = default!;
+        public decimal GarageHourlyRate { get; set; }
+        public VehicleViewModel Vehicle { get; set; } = default!;
+        public MemberViewModel Member { get; set; } = default!;
     }
 }
