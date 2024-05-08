45e314c811732cb80490026e47203fedecd89d2d
suny-am <visualarea.1@gmail.com>
2024-05-08 17:54:48 +0000

    Add Garage Validations; Add Hourly Rate to Garage Create

--- GarageThree.Web/ViewModels/Garage/GarageCreateOrEditViewModel.cs
+++ GarageThree.Web/ViewModels/Garage/GarageCreateOrEditViewModel.cs
@@ -1,13 +1,20 @@
-﻿namespace GarageThree.Web.ViewModels.Garage
+﻿namespace GarageThree.Web.ViewModels.Garage;
+
+public class GarageCreateOrEditViewModel
 {
-    public class GarageCreateOrEditViewModel
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }  = default!;
+    [Required]
+    [MinLength(5, ErrorMessage = "Username must be at least 5 charactes")]
+    [MaxLength(50, ErrorMessage = "Username cannot exceed 50 characters")]
+    [RegularExpression(@"[a-zA-Z\s0-9]*", ErrorMessage = "Only alphabetical characters and digits allowed")]
+    public string Name { get; set; } = default!;
 
-        [Range(5, 100)]
-        public int Capacity { get; set; }
-    }
+    [Required]
+    [Range(5, 100, ErrorMessage = "Value must between 5-100")]
+    public int Capacity { get; set; } = 5;
+
+    [Required]
+    [Range(10, 30, ErrorMessage = "Value must between 10-30")]
+    public int HourlyRate { get; set; } = 10;
 }
