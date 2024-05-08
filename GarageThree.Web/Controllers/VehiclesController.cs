685afc18e63539ecb5d7539fa8a1286f0aecf5a3
John Källström <johnkaellstrom@gmail.com>
2024-05-08 09:32:53 +0200

    Fix build fail

--- GarageThree.Web/Controllers/VehiclesController.cs
+++ GarageThree.Web/Controllers/VehiclesController.cs
@@ -5,24 +5,21 @@ public class VehiclesController(IMapper mapper, IRepository<Vehicle> vehicleRepo
     private readonly IMapper _mapper = mapper;
     private readonly IRepository<Vehicle> _vehicleRepository = vehicleRepository;
 
-        public async Task<IActionResult> Index(int? garageId, string? searchTerm)
+    public async Task<IActionResult> Index(int? garageId, string? searchTerm)
+    {
+        var vehicles = await _vehicleRepository.Filter(new QueryParams
         {
-            var vehicles = await _repository.Filter(new QueryParams
-            {
-                Id = garageId,
-                SearchTerm = searchTerm
-            });
+            Id = garageId,
+            SearchTerm = searchTerm
+        });
 
         VehicleIndexViewModel viewModel = new()
         {
             Vehicles = _mapper.Map<IEnumerable<VehicleViewModel>>(vehicles)
         };
-        
+
         if (garageId.HasValue) viewModel.GarageId = garageId.Value;
 
-            return View(viewModel);
-        }
-
-        public IActionResult Clear() => RedirectToAction(nameof(Index), new { });
+        return View(viewModel);
     }
 }
