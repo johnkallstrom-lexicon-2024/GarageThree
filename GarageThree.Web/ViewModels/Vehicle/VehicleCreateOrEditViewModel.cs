29347c7ea0eccb207c86326d0bf701394cf5e659
John Källström <johnkaellstrom@gmail.com>
2024-05-08 11:50:04 +0200

    Vehicle Create form

--- GarageThree.Web/ViewModels/Vehicle/VehicleCreateOrEditViewModel.cs
+++ GarageThree.Web/ViewModels/Vehicle/VehicleCreateOrEditViewModel.cs
@@ -3,15 +3,25 @@
     public class VehicleCreateOrEditViewModel
     {     
         [Required]
+        [DisplayName("Registration Number")]
         public string RegNumber { get; set; } = default!;
+
         [Required]
         public string Brand { get; set; }  = default!;
+
         [Required]
         public string Model { get; set; }  = default!;
+
         [Required]
+        [DisplayName("Parked At")]
         public DateTime RegisterdAt { get; set; } = DateTime.Now;
+
         [Required]
-        public int MemberId { get; set; } 
+        public int MemberId { get; set; }
+
+        [Required]
+        public int GarageId { get; set; }
+
         [Required]
         public Color Color { get; set; }
     }
