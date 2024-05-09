7510409a62e3b8fad33e36d8456a6fddf63e9bcb
John Källström <johnkaellstrom@gmail.com>
2024-05-09 10:53:43 +0200

    Add migration

--- GarageThree.Persistence/Migrations/20240509085221_Checkout.Designer.cs
+++ GarageThree.Persistence/Migrations/20240509085221_Checkout.Designer.cs
@@ -4,6 +4,7 @@ using GarageThree.Persistence.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GarageThree.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240509085221_Checkout")]
+    partial class Checkout
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
