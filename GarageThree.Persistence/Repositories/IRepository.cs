ce3fdee635c68628b2825d714a95820eef09a35a
John Källström <johnkaellstrom@gmail.com>
2024-05-09 10:12:01 +0200

    Any as Generic

--- GarageThree.Persistence/Repositories/IRepository.cs
+++ GarageThree.Persistence/Repositories/IRepository.cs
@@ -9,9 +9,9 @@ public interface IRepository<T> where T : class
     public Task<T?> Delete(int id);
     public Task<T?> GetById(int id);
     bool Any();
-    bool Any(Func<Vehicle, bool> predicate);
+    bool Any(Func<T, bool> predicate);
     public Task<bool> AnyAsync();
-    public Task<bool> AnyAsync(Expression<Func<Vehicle, bool>> predicate);
+    public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
     public Task<IEnumerable<T>> GetAll();
     public Task<IEnumerable<T>> Filter(QueryParams parameters);
     public Task<T?> Single(QueryParams parameters);
